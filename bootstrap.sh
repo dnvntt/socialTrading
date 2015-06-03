835e5d13ac6c6d05c188f97f9ce14fcd14d24e3c
Trung Ngo <ndtrung4419@gmail.com>
2015-06-03 20:32:45 +0700

    Update the Vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,19 +2,30 @@
 
 yum -y update
 
+# Fix a virtualbox driver issue after updating
+yum install -y kernel-devel-$(uname -r) kernel-headers-$(uname -r) dkms
+/etc/init.d/vboxadd setup
+
 yum install -y vim man byobu
 yum groupinstall -y 'Development Tools'
 
 
-# Nodejs
+# NodeJS
 curl -sL https://rpm.nodesource.com/setup | bash -
 yum install -y nodejs
-npm install -g react-tools
+npm install -g react-tools bower
 
 
-# RabbitMQ
-wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-rpm epel-release-6*.rpm
-wget http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.2/rabbitmq-server-3.2.2-1.noarch.rpm
+# Installing RabbitMQ
+wget --continue http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum install -y epel-release-6*.rpm
+wget --continue http://www.rabbitmq.com/releases/rabbitmq-server/v3.2.2/rabbitmq-server-3.2.2-1.noarch.rpm
 rpm --import http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-yum install rabbitmq-server-3.2.2-1.noarch.rpm
+yum install -y rabbitmq-server-3.2.2-1.noarch.rpm
+
+# Enable the management plugin
+rabbitmq-plugins enable rabbitmq_management
+
+# Make it start on startup
+chkconfig rabbitmq-server on
+/sbin/service rabbitmq-server start
