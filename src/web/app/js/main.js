437a1b935891e4808e6bc188a4ef33f6f3623db6
Trung Ngo <ndtrung4419@gmail.com>
2015-06-03 20:35:16 +0700

    New front-end workflow with Makefile

--- src/web/app/js/main.js
+++ src/web/app/js/main.js
@@ -1,73 +1,84 @@
 'use strict';
 
-(function() {
-    var Traders = Backbone.Collection.extend({
-        url: "/api/traders"
-    });
+var React = require("react");
+var Backbone = require("backbone");
+var _ = require("underscore");
 
-    var TraderLine = React.createClass({
-        follow: function() {
-            alert("Following trader " + this.props.trader.id)
-        },
 
-        render: function() {
+var Traders = Backbone.Collection.extend({
+    url: "/api/traders"
+});
+
+var traders = new Traders();
+
+
+var TraderLine = React.createClass({
+    follow: function() {
+        alert("Following trader " + this.props.trader.id)
+    },
+
+    render: function() {
+        console.log(this.props.trader);
+        return (
+            <div className="trader clearfix">
+            <div className="block">
+            <img src="/img/trader1.jpg" className="img-thumbnail"/>
+            </div>
+
+            <div className="block basic-info">
+            <h2>{this.props.trader.get("name")}</h2>
+            <p className="text-muted">{this.props.trader.get("description")}</p>
+            </div>
+
+            <div className="block">
+            <span className="text-label">Amount following</span><br/>
+            <span>${this.props.trader.get("amountFollowing")}</span><br/>
+
+            <span className="text-label">People following</span><br/>
+            <span>{this.props.trader.get("peopleFollowing")}</span><br/>
+
+            <span className="text-label">ROI</span><br/>
+            <span>{this.props.trader.get("roi")}%</span>
+            </div>
+
+            <div className="block">
+            <button type="submit" 
+            className="btn btn-primary"
+            onClick={this.follow}>Follow</button>
+            </div>
+            </div>
+        );
+    }
+});
+
+var TraderList = React.createClass({
+    componentDidMount: function() {
+        var _this = this;
+        this.props.traders.on("add remove reset", function() {
+            console.log("New data");
+            _this.forceUpdate();
+        });
+    },
+
+    render: function() {
+        var nodes = this.props.traders.map(function(trader) {
             return (
-                <div className="trader clearfix">
-                <div className="block">
-                <img src="/img/trader1.jpg" className="img-thumbnail"/>
-                </div>
-
-                <div className="block basic-info">
-                <h2>{this.props.trader.name}</h2>
-                <p className="text-muted">{this.props.trader.description}</p>
-                </div>
-
-                <div className="block">
-                <span className="text-label">Amount following</span><br/>
-                <span>${this.props.trader.amountFollowing}</span><br/>
-
-                <span className="text-label">People following</span><br/>
-                <span>{this.props.trader.peopleFollowing}</span><br/>
-
-                <span className="text-label">ROI</span><br/>
-                <span>{this.props.trader.roi}%</span>
-                </div>
-
-                <div className="block">
-                <button type="submit" 
-                className="btn btn-primary"
-                onClick={this.follow}>Follow</button>
-                </div>
-                </div>
+                <TraderLine trader={trader}/>
             );
-        }
-    });
+        });
 
-    var TraderList = React.createClass({
-        render: function() {
-            var traders = [
-                {name: "Trung", description: "Haha", amountFollowing: 999, peopleFollowing: 34, roi: 344, id: 3434},
-                {name: "Trung", description: "Haha", amountFollowing: 999, peopleFollowing: 34, roi: 344, id: 3434},
-                {name: "Trung", description: "Haha", amountFollowing: 999, peopleFollowing: 34, roi: 344, id: 3434},
-            ];
-            var traderNodes = [
-                <TraderLine trader={traders[0]}/>,
-                <TraderLine trader={traders[1]}/>, 
-                <TraderLine trader={traders[2]}/>
-            ];
-
-            return (
-                <div className="trader-list">
-                {traderNodes}
-                </div>
-            );
-        }
-    });
+        return (
+            <div className="trader-list">
+            {nodes}
+            </div>
+        );
+    }
+});
 
 
-    React.render(
-        <TraderList/>,
-        document.getElementById('example')
-    );
+React.render(
+    <TraderList traders={traders}/>,
+    document.getElementById('example')
+);
 
-})();
+traders.fetch();
