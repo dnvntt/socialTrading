14755cff3fc7069a9ecc4409ee7e0e2ed341b654
Trung Ngo <ndtrung4419@gmail.com>
2015-06-08 13:46:19 +0700

    [frontend] navbar

--- src/main/webapp/app/js/main.js
+++ src/main/webapp/app/js/main.js
@@ -198,6 +198,32 @@ var TraderList = React.createClass({
     }
 });
 
+var NavBar = React.createClass({
+    render: function() {
+        return (
+            <nav className="navbar navbar-default">
+                <div className="container">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="#">
+                           <img alt="Brand" 
+                           width="20"
+                           height="20"
+                           src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAoCAMAAAC7IEhfAAAA81BMVEX///9VPnxWPXxWPXxWPXxWPXxWPXxWPXz///9hSYT6+vuFc6BXPn37+vz8+/z9/f2LeqWMe6aOfqiTg6uXiK5bQ4BZQX9iS4VdRYFdRYJfSINuWI5vWY9xXJF0YJR3Y5Z4ZZd5ZZd6Z5h9apq0qcW1qsW1q8a6sMqpnLyrn76tocCvpMGwpMJoUoprVYxeRoJjS4abjLGilLemmbrDutDFvdLPx9nX0eDa1OLb1uPd1+Td2OXe2eXh3Ofj3+nk4Orl4evp5u7u7PLv7fPx7/T08vb08/f19Pf29Pj39vn6+fuEcZ9YP35aQn/8/P1ZQH5fR4PINAOdAAAAB3RSTlMAIWWOw/P002ipnAAAAPhJREFUeF6NldWOhEAUBRvtRsfdfd3d3e3/v2ZPmGSWZNPDqScqqaSBSy4CGJbtSi2ubRkiwXRkBo6ZdJIApeEwoWMIS1JYwuZCW7hc6ApJkgrr+T/eW1V9uKXS5I5GXAjW2VAV9KFfSfgJpk+w4yXhwoqwl5AIGwp4RPgdK3XNHD2ETYiwe6nUa18f5jYSxle4vulw7/EtoCdzvqkPv3bn7M0eYbc7xFPXzqCrRCgH0Hsm/IjgTSb04W0i7EGjz+xw+wR6oZ1MnJ9TWrtToEx+4QfcZJ5X6tnhw+nhvqebdVhZUJX/oFcKvaTotUcvUnY188ue/n38AunzPPE8yg7bAAAAAElFTkSuQmCC"/>
+                        </a>
+                    </div>
+
+                    <nav>
+                        <ul className="nav navbar-nav navbar-right">
+                        <li><p className="navbar-text">Hello, {me.id}</p></li>
+                        <li><button className="btn btn-default navbar-btn">Logout</button></li>
+                        </ul>
+                    </nav>
+                </div>
+            </nav>
+        );
+    }
+});
+
 var App = React.createClass({
     componentDidMount: function() {
         var _this = this;
@@ -229,28 +255,26 @@ var App = React.createClass({
     },
 
     logout: function() {
-
+        alert("Logging out is not implemented yet!");
     },
 
     render: function() {
-        var inner;
-
         if (this.state.authChecking) {
-            inner = (<h2>Checking authentication...</h2>);
+            return (<h2>Checking authentication...</h2>);
         } else {
             if (this.state.loggedIn) {
-                <button onClick={this.logout}>Logout</button>
-                inner = <TraderList traders={traders}/>
+                return (
+                    <div>
+                        <NavBar/>
+                        <div className="container">
+                        <TraderList traders={traders}/>
+                        </div>
+                    </div>
+                );
             } else {
-                inner = <LoginScreen/>;
+                return <LoginScreen/>;
             }
         }
-
-        return (
-            <div>
-                {inner}
-            </div>
-        );
     }
 });
 
