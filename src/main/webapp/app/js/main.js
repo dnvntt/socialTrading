0c6383c8cd99a6d7f1b871eb966066d7f98e9147
Trung Ngo <ndtrung4419@gmail.com>
2015-06-08 13:14:34 +0700

    [frontend] Follow and unfollow trader

--- src/main/webapp/app/js/main.js
+++ src/main/webapp/app/js/main.js
@@ -1,11 +1,70 @@
 'use strict';
 
 var Traders = Backbone.Collection.extend({
-    url: "/api/v1/traders"
+    url: "/api/v1/traders",
+});
+
+var FollowingRels = Backbone.Collection.extend({
+    model: Backbone.Model.extend({
+        idAttribute: "traderId"
+    });
+});
+
+var Follower = Backbone.Model.extend({
+    defaults: {
+        followingTraders: new FollowingRels()
+    },
+
+    initialize: function() {
+        var _this = this;
+
+        dispatcher.register(function(message) {
+            switch (message.type) {
+            case "ask_to_follow_trader":
+                $.post("/api/v1/follower/" + _this.id + "/following", {
+                   traderid: message.trader.id,
+                   money: 213,
+                   maxopen: 3
+                }).then(function() {
+                    _this.fetch();
+                    traders.fetch();
+                });
+                break;
+            case "ask_to_unfollow_trader":
+                $.ajax({
+                    url: "/api/v1/follower/" + _this.id + "/following/" + message.trader.id,
+                    method: "DELETE",
+                    success: function() {
+                        _this.fetch();
+                        traders.fetch();
+                    }
+                });
+                break;
+            }
+        });
+    },
+
+    parse: function(res) {
+        var _this = this;
+        console.log("parsing follower");
+        this.get("followingTraders").url = "/api/v1/follower/" + this.id + "/following";
+        this.get("followingTraders").fetch().then(function() {
+            console.log("followingTraders fetched")
+            _this.get("followingTraders").trigger("change")
+        });
+        return res;
+    },
+
+    isFollowing: function(trader) {
+        return this.get("followingTraders").get(trader.get("id")) !== undefined;
+    }
 });
 
 var dispatcher = new Flux.Dispatcher();
 var traders = new Traders();
+var me = new Follower();
+me.url = "/api/v1/me";
+me.fetch();
 
 dispatcher.register(function(message) {
     console.log(message);
@@ -13,11 +72,31 @@ dispatcher.register(function(message) {
 
 
 var TraderLine = React.createClass({
-    follow: function() {
-        alert("Following trader " + this.props.trader.id)
+    followBtnToggled: function() {
+        if (me.isFollowing(this.props.trader)) {
+            dispatcher.dispatch({
+                type: "ask_to_unfollow_trader",
+                trader: this.props.trader
+            });
+        } else {
+            dispatcher.dispatch({
+                type: "ask_to_follow_trader",
+                trader: this.props.trader
+            });
+        }
     },
 
     render: function() {
+        var followBtnClasses = "btn ";
+        if (me.isFollowing(this.props.trader)) {
+            var followButtonText = "Unfollow";
+            followBtnClasses += "btn-danger";
+        } else {
+            followButtonText = "Follow";
+            followBtnClasses += "btn-primary";
+        }
+        console.log(this.props.trader)
+
         return (
             <div className="trader clearfix">
             <div className="block">
@@ -26,12 +105,15 @@ var TraderLine = React.createClass({
 
             <div className="block basic-info">
             <h2>{this.props.trader.get("name")}</h2>
-            <p className="text-muted">{this.props.trader.get("description")}</p>
+            <p className="text-muted">
+                {this.props.trader.get("id")}
+                {this.props.trader.get("description")}
+            </p>
             </div>
 
             <div className="block">
             <span className="text-label">Amount following</span><br/>
-            <span>${this.props.trader.get("amountFollowing")}</span><br/>
+            <span>${this.props.trader.get("cash")}</span><br/>
 
             <span className="text-label">People following</span><br/>
             <span>{this.props.trader.get("peopleFollowing")}</span><br/>
@@ -42,8 +124,8 @@ var TraderLine = React.createClass({
 
             <div className="block">
             <button type="submit" 
-            className="btn btn-primary"
-            onClick={this.follow}>Follow</button>
+            className={followBtnClasses}
+            onClick={this.followBtnToggled}>{{followButtonText}}</button>
             </div>
             </div>
         );
@@ -54,7 +136,7 @@ var RiskSlider = React.createClass({
     // TODO: Detect when the user stops sliding then send the new value to server
     getInitialState: function() {
         return {
-            value: 50  
+            value: 50
         };
     },
 
@@ -85,7 +167,17 @@ var RiskSlider = React.createClass({
 var TraderList = React.createClass({
     componentDidMount: function() {
         var _this = this;
-        this.props.traders.on("add remove reset", function() {
+
+        me.on("change add remove", function() {
+            _this.forceUpdate();
+        });
+
+        me.get("followingTraders").on("change", function() {
+            console.log("followingTraders changed")
+            _this.forceUpdate();
+        });
+
+        this.props.traders.on("change add remove reset", function() {
             _this.forceUpdate();
         });
     },
@@ -106,9 +198,90 @@ var TraderList = React.createClass({
     }
 });
 
+var App = React.createClass({
+    componentDidMount: function() {
+        var _this = this;
+        dispatcher.register(function(message) {
+            switch(message.type) {
+            case "auth.authenticated":
+                _this.setState({authChecking: false, loggedIn: true});
+                _this.forceUpdate();
+            }
+        });
+    },
+
+    getInitialState: function() {
+        var _this = this;
+        $.get("/api/v1/me").then(
+            function() {
+                _this.setState({authChecking: false, loggedIn: true});
+                _this.forceUpdate();
+            }).fail(
+            function() {
+                _this.setState({authChecking: false, loggedIn: false});
+                _this.forceUpdate();
+            });
+
+        return {
+            authChecking: true,
+            loggedIn: false
+        };
+    },
+
+    logout: function() {
+
+    },
+
+    render: function() {
+        var inner;
+
+        if (this.state.authChecking) {
+            inner = (<h2>Checking authentication...</h2>);
+        } else {
+            if (this.state.loggedIn) {
+                <button onClick={this.logout}>Logout</button>
+                inner = <TraderList traders={traders}/>
+            } else {
+                inner = <LoginScreen/>;
+            }
+        }
+
+        return (
+            <div>
+                {inner}
+            </div>
+        );
+    }
+});
+
+var LoginScreen = React.createClass({
+    login: function() {
+        $.post("/api/v1/login", {
+            "user": this.refs.username.getDOMNode().value,
+            "pass": this.refs.password.getDOMNode().value
+        }).then(function() {
+            dispatcher.dispatch({
+                type: "auth.authenticated"
+            });
+        }).fail(function() {
+            alert("NOPE!")
+        });
+    },
+
+    render: function() {
+        return (
+        <div>
+            <input type="text" ref="username" placeholder="Username"/>
+            <input type="password" ref="password" placeholder="Password"/>
+            <input type="submit" value="Login" onClick={this.login}/>
+        </div>
+        );
+    }
+});
+
 
 React.render(
-    <TraderList traders={traders}/>,
+    <App/>,
     document.getElementById('example')
 );
 
